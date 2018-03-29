fbb2ca61bd3eaa54edb3f2c7ecf240fe4248aff0
Curtis Mak <makcurtis@gmail.com>
2018-03-29 11:29:09 -0700

    First commit

---
+++ CoreUtils/Util.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace CoreUtils
+{
+    public class Util
+    {
+        public static async void deleteTempClips()
+        {
+            StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
+            try
+            {
+                storageFolder = await storageFolder.GetFolderAsync("TempClip");
+            }
+            catch (IOException ex)
+            {
+                return;
+            }
+
+            IReadOnlyList<IStorageItem> itemsList = await storageFolder.GetItemsAsync();
+            foreach (var item in itemsList)
+            {
+                if (item is StorageFile)
+                {
+                    await item.DeleteAsync();
+                }
+            }
+        }
+    }
+}
